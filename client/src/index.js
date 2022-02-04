e1f521464f4c178277d448c2c12f13b19fb3b57b
bipinrai054 <bipinrai054@gmail.com>
2022-02-05 02:17:31 +0545

    redux initialized

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
+// react
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+// components
 import App from './App';
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
